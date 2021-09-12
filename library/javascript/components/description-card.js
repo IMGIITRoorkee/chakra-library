1285eed869293850d1d567e4064758ace1cf44b7
rochakjain361 <rochak_r@ec.iitr.ac.in>
2021-09-12 12:48:03 +0530

    Add description-card.css in index.css and minor patches

--- library/javascript/components/description-card.js
+++ library/javascript/components/description-card.js
@@ -1,32 +1,32 @@
-var imagePath = "../../library/assets/images/description-card/right-arrow.svg";
-var imagePathInverse =
+let imagePath = "../../library/assets/images/description-card/right-arrow.svg";
+let imagePathInverse =
   "../../library/assets/images/description-card/right-arrow-inverted.svg";
 
-var descriptionCard = document.querySelectorAll(".ui.description-card");
+let descriptionCard = document.querySelectorAll(".ui.description-card");
 
-var descriptionCardInverted = document.querySelectorAll(
+let descriptionCardInverted = document.querySelectorAll(
   ".ui.description-card.inverted"
 );
 
-for (var i = 0; descriptionCard != -null && i < descriptionCard.length; i++) {
-  var arrows = descriptionCard[i].getElementsByClassName("right arrow");
-  for (var j = 0; arrows !== null && j < arrows.length; j++) {
+for (let i = 0; descriptionCard !== null && i < descriptionCard.length; i++) {
+  let arrows = descriptionCard[i].getElementsByClassName("right arrow");
+  for (let j = 0; arrows !== null && j < arrows.length; j++) {
     arrows[j].innerHTML = "";
-    var arrow = document.createElement("img");
+    let arrow = document.createElement("img");
     arrow.setAttribute("src", imagePath);
     arrows[j].appendChild(arrow);
   }
 }
 
 for (
-  var i = 0;
-  descriptionCardInverted != -null && i < descriptionCardInverted.length;
+  let i = 0;
+  descriptionCardInverted !== null && i < descriptionCardInverted.length;
   i++
 ) {
-  var arrows = descriptionCardInverted[i].getElementsByClassName("right arrow");
-  for (var j = 0; arrows !== null && j < arrows.length; j++) {
+  let arrows = descriptionCardInverted[i].getElementsByClassName("right arrow");
+  for (let j = 0; arrows !== null && j < arrows.length; j++) {
     arrows[j].innerHTML = "";
-    var arrow = document.createElement("img");
+    let arrow = document.createElement("img");
     arrow.setAttribute("src", imagePathInverse);
     arrows[j].appendChild(arrow);
   }
