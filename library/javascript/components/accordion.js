207febba86cb003fc3226b66078819e603c660ea
Shashank Aital <aitalshashank2@gmail.com>
2021-09-14 13:17:14 +0530

    Implement image accordion Changes: 1. Add CSS and JS for image accordion component 2. Add test file for image accordion component

--- library/javascript/components/accordion.js
+++ library/javascript/components/accordion.js
@@ -2,18 +2,18 @@ var downArrow = "../../library/assets/images/accordion/down-arrow.svg"
 var upArrow = "../../library/assets/images/accordion/up-arrow.svg"
 
 var accordions = document.querySelectorAll('.ui.accordion')
-for(var i = 0; (accordions !== null) && (i < accordions.length); i++){
-    
+for (var i = 0; (accordions !== null) && (i < accordions.length); i++) {
+
     var categories = accordions[i].getElementsByClassName('category');
-    for(var j = 0; (categories !== null) && (j < categories.length); j++){
+    for (var j = 0; (categories !== null) && (j < categories.length); j++) {
 
         // Add alternate gray effect
-        if(j%2 === 1){
+        if (j % 2 === 1) {
             categories[j].classList += ' gray'
         }
 
         var items = categories[j].getElementsByClassName('item')
-        for(var k = 0; (items !== null) && (k < items.length); k++){
+        for (var k = 0; (items !== null) && (k < items.length); k++) {
 
             // Add dividers
             const divider = document.createElement('div')
@@ -21,7 +21,7 @@ for(var i = 0; (accordions !== null) && (i < accordions.length); i++){
             items[k].parentNode.insertBefore(divider, items[k].nextSibling)
 
             var titles = items[k].getElementsByClassName('title')
-            if(titles !== null){
+            if (titles !== null) {
                 var title = titles[0]
 
                 // Add Arrow
@@ -31,19 +31,19 @@ for(var i = 0; (accordions !== null) && (i < accordions.length); i++){
                 title.appendChild(arrow)
 
                 // Manage click
-                title.addEventListener('click', function(e){
+                title.addEventListener('click', function (e) {
                     e.stopPropagation()
                     e.preventDefault()
 
-                    if(e.target){
+                    if (e.target) {
                         const description = e.target.nextElementSibling
                         const arrow = e.target.childNodes[1]
-                        
-                        if(description !== null){
-                            if(description.style.display === 'block'){
+
+                        if (description !== null) {
+                            if (description.style.display === 'block') {
                                 description.style.display = 'none'
                                 arrow.setAttribute('src', downArrow)
-                            }else{
+                            } else {
                                 description.style.display = 'block'
                                 arrow.setAttribute('src', upArrow)
                             }
@@ -52,6 +52,62 @@ for(var i = 0; (accordions !== null) && (i < accordions.length); i++){
                 })
             }
 
+        }
+
+    }
+
+}
+
+
+var imageAccordions = document.querySelectorAll('.ui.image-accordion')
+for (var i = 0; (imageAccordions !== null) && (i < imageAccordions.length); i++) {
+
+    const items = imageAccordions[i].getElementsByClassName('item')
+    for (var j = 0; (items !== null) && (j < items.length); j++) {
+
+        const contents = items[j].getElementsByClassName('content')
+        if (contents !== null) {
+            const content = contents[0]
+
+            const titles = content.getElementsByClassName('title')
+            if (titles !== null) {
+                const title = titles[0]
+
+                // Add Arrow
+                const arrow = document.createElement('img')
+                arrow.setAttribute('src', downArrow)
+                arrow.className = 'arrow'
+                title.appendChild(arrow)
+
+                // Add divider
+                const divider = document.createElement('div')
+                divider.className = 'divider'
+                title.parentNode.insertBefore(divider, title.nextSibling)
+
+                // Manage click
+                title.addEventListener('click', function (e) {
+                    e.stopPropagation()
+                    e.preventDefault()
+
+                    if (e.target) {
+                        const divider = e.target.nextElementSibling
+                        const description = divider.nextElementSibling
+                        const arrow = e.target.childNodes[1]
+
+                        if (description !== null) {
+                            if (description.style.display === 'block') {
+                                divider.style.display = 'none'
+                                description.style.display = 'none'
+                                arrow.setAttribute('src', downArrow)
+                            } else {
+                                divider.style.display = 'block'
+                                description.style.display = 'block'
+                                arrow.setAttribute('src', upArrow)
+                            }
+                        }
+                    }
+                })
+            }
         }
 
     }
