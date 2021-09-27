bfd904801ae1bf2cf06962d3aaea954365c2af63
Prernaswarna <prernaswarnakumar@gmail.com>
2021-09-27 12:36:35 +0530

    Add components

---
+++ library/javascript/components/complete-info-card.js
@@ -0,0 +1,6 @@
+let completeInfoCards = document.querySelectorAll(".ui.complete-info-card");
+
+for (let card of completeInfoCards) {
+	  let flex = card.querySelector(".flex");
+	  let info = flex.querySelector(".info");
+}
