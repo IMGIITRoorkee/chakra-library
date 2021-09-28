79387bf76d50a426577854aa46c1bb4add410594
Prernaswarna <prernaswarnakumar@gmail.com>
2021-09-28 12:43:21 +0530

    Reverting changes

--- library/javascript/components/complete-info-card.js
+++
@@ -1,6 +0,0 @@
-let completeInfoCards = document.querySelectorAll(".ui.complete-info-card");
-
-for (let card of completeInfoCards) {
-	  let flex = card.querySelector(".flex");
-	  let info = flex.querySelector(".info");
-}
