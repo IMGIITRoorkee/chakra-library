962a6b37d244f4e0ea3e880173b5a020617a32a0
Prernaswarna <prernaswarnakumar@gmail.com>
2021-09-28 19:02:20 +0530

    Minor changes
    Added new line in stats-card.css stats-card.html and index.html

--- library/javascript/components/stats-card.js
+++ library/javascript/components/stats-card.js
@@ -1,14 +1,14 @@
 let statsCards = document.querySelectorAll(".ui.stats-card");
 
 for (let card of statsCards) {
-	  let flex = card.querySelector(".flex");
-	  let info = flex.querySelector(".info");
+	let flex = card.querySelector(".flex");
+	let info = flex.querySelector(".info");
 
-	  for (let key of Object.keys(paths)) {
-		      let keyNode = info.querySelector(`.${key}`);
-		      let imageContainer = keyNode.querySelector(".icon");
-		      let image = document.createElement("img");
-		      image.setAttribute("src", paths[key]);
-		      imageContainer.append(image);
-		    }
+	for (let key of Object.keys(paths)) {
+		let keyNode = info.querySelector(`.${key}`);
+		let imageContainer = keyNode.querySelector(".icon");
+		let image = document.createElement("img");
+		image.setAttribute("src", paths[key]);
+		imageContainer.append(image);
+	}
 }
