584714f22775c276e3b0142788efbc3acea9ba23
Prernaswarna <prernaswarnakumar@gmail.com>
2021-10-12 19:01:09 +0530

    Add attribute data-target for links

--- library/javascript/components/link.js
+++ library/javascript/components/link.js
@@ -1,11 +1,21 @@
-let link = document.querySelectorAll(".ui.link");
-for (var i = 0; (link !== null) && (i < link.length); i++) {
-  const items = link[i].getElementsByClassName('button')
-  for (var j = 0; (items !== null) && (j < items.length); j++) {
-    items[j].onclick = changeLink;
-    
+let buttonLinks = document.querySelectorAll(".button.link");
+
+for (let link of buttonLinks) {
+    var key = link.getAttribute("data-target");
+    link.onclick = changeLink;
+
     function changeLink() {
-      window.location.href = 'https://google.com';
+        window.location.href = key;
     }
-  }
+}
+
+let textLinks = document.querySelectorAll(".text.link");
+
+for (let link of textLinks) {
+    var key = link.getAttribute("data-target");
+    link.onclick = changeLink;
+
+    function changeLink() {
+        window.location.href = key;
+    }
 }
