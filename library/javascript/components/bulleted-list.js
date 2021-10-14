ecd699e591f9a1252e908630cbcdd1f039100afb
saurabhhere <saurabhguptajpr@yahoo.in>
2021-10-14 12:50:25 +0530

    Make bulleted list component

---
+++ library/javascript/components/bulleted-list.js
@@ -0,0 +1,21 @@
+const bulletedLists = document.querySelectorAll('.ui.bulleted-list')
+
+for (let i=0; (bulletedLists !== null) && (i < bulletedLists.length); i++){
+    let icon;
+    if (bulletedLists[i].hasAttribute("data-icon")){
+        icon = bulletedLists[i].getAttribute("data-icon");
+    } else {
+        if (bulletedLists[i].classList.contains('inverted')){
+            icon = "arrow_dark";
+        } else {
+            icon = "arrow";
+        }
+    }
+    let bulletedItems = bulletedLists[i].getElementsByClassName('bulletedItem');
+    for (let j=0; (bulletedItems !== null) && (j < bulletedItems.length); j++) {
+        const image = document.createElement("img");
+        image.setAttribute("src", paths[icon]);
+        image.classList.add("icon_image");
+        bulletedItems[j].prepend(image);
+    }
+}
