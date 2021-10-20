a6257da6aa8d753a52608d9d9a2d53fe524abd1d
Kunal <khandelwal.kunal4@gmail.com>
2021-10-20 11:08:28 +0530

    Minor fix and add color variables

--- library/javascript/components/quote-card.js
+++ library/javascript/components/quote-card.js
@@ -1,19 +1,12 @@
-let imageConatiner = document.querySelectorAll(".ui.quote-card .picture");
+let imageConatiner = document.querySelectorAll(".ui.quote-card .picture")[0];
 
 let default_faculty = "../../library/assets/icons/default_faculty.png";
 
-for(let quote of imageConatiner){
-    let path=quote.getAttribute("src");
-    print(path)
-    if(path==""){
-        path=default_faculty;
-    }
-    let circle = document.createElement("div");
-    circle.style.width="111px";
-    circle.style.height="111px";
-    circle.style.borderRadius="50%";
-    circle.style.backgroundRepeat="no-repeat";
-    circle.style.backgroundPosition="center";
-    circle.style.backgroundImage=`url(${path})`;
-    quote.appendChild(circle);
+let path=imageConatiner.getAttribute("src");
+
+if(path==""){
+    path=default_faculty;
 }
+
+let circle = imageConatiner.querySelector(".photo")
+circle.style.backgroundImage=`url(${path})`;
