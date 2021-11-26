790486a20b2a45044a21bbc602bc9cdb99c37904
Kunal <khandelwal.kunal4@gmail.com>
2021-11-26 17:41:07 +0530

    Social icon library included(partial)

---
+++ library/javascript/components/social-icon.js
@@ -0,0 +1,19 @@
+let socialIconToNormalIcon =  new Map();
+socialIconToNormalIcon.set('facebook_icon', 'facebook_logo');
+socialIconToNormalIcon.set('twitter_icon', 'twitter_logo');
+socialIconToNormalIcon.set('instagram_icon', 'instagram_logo');
+socialIconToNormalIcon.set('youtube_icon', 'youtube_logo');
+socialIconToNormalIcon.set('linkedin_icon', 'linkedin_logo');
+socialIconToNormalIcon.set('web_icon', 'web_logo');
+
+let links = document.querySelectorAll(".ui.social-icon");
+console.log(links); 
+for (let link of links) {
+    let a = link.classList[2];
+    
+    let image = document.createElement("img");
+    image.className = 'ui icon';
+    image.setAttribute('data-icon', socialIconToNormalIcon.get(a));
+    link.appendChild(image);
+
+}
