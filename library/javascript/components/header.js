3e0344c2e13cf2a23211dd4d032ce07b3ca5ae0d
Ayush Bansal <ayu023ban@gmail.com>
2021-12-03 21:33:42 +0530

    Make Header component
    - Make responsive header for main page, department page

---
+++ library/javascript/components/header.js
@@ -0,0 +1,10 @@
+// let wrapper = document.querySelector(".menuWrapper");
+// let nav = wrapper.firstElementChild;
+// let ul = nav.firstElementChild;
+// // console.log(ul)
+// let li = ul.children;
+// for (let m of li) {
+//   m.addEventListener("mouseenter", () => {
+//     console.log(m.innerText);
+//   });
+// }
