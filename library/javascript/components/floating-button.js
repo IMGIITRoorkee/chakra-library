30fef55396d465b384643210ce0363c2ab1fc766
Saurabh <saurabhguptajpr@yahoo.in>
2021-12-05 23:23:02 +0530

    Make floating button

---
+++ library/javascript/components/floating-button.js
@@ -0,0 +1,24 @@
+function hover(element) {
+  element.setAttribute("src", paths.floating_button_hover);
+}
+
+function unhover(element) {
+  element.setAttribute("src", paths.floating_button);
+}
+
+function goToTop(){
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+window.onscroll = function() {showScroll()};
+
+var scrollBtn = document.querySelector(".ui.floating-btn .ui.icon");
+
+function showScroll() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+}
