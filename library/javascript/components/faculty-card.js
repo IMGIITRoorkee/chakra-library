bff46ba6043ae462b9f1b856a9d4e7889cad98d1
Rishi Ranjan <43873720+rish9101@users.noreply.github.com>
2021-12-15 15:16:39 -0500

    Fix faculty card to use typography and link components
    * Fix faculty card
    
    * Update Read More Links in Faculty Card to Link component

--- library/javascript/components/faculty-card.js
+++ library/javascript/components/faculty-card.js
@@ -7,17 +7,6 @@ let facultyCardIconMap = {
 let facultyCards = document.querySelectorAll(".ui.faculty-card");
 
 for (let card of facultyCards) {
-  let flex = card.querySelector(".flex");
-  let info = flex.querySelector(".info");
-
-  for (let key of Object.keys(facultyCardIconMap)) {
-    let keyNode = info.querySelector(`.${key}`);
-    let imageContainer = keyNode.querySelector(".icon");
-    let image = document.createElement("img");
-    image.className='ui icon'
-    image.setAttribute("data-icon", facultyCardIconMap[key]);
-    imageContainer.append(image);
-  }
   let phoneNode = info.querySelector(".phone");
   let phoneText = phoneNode.querySelector(".text");
   if (phoneText.innerHTML == "") phoneText.innerHTML = "--";
