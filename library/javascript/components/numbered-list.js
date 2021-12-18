a6037bd7f427960dbdd1a91b3ffb3a99783912fa
Shashank Aital <aitalshashank2@gmail.com>
2021-12-18 15:23:03 +0530

    [Numbered List] Reuse components

--- library/javascript/components/numbered-list.js
+++ library/javascript/components/numbered-list.js
@@ -2,13 +2,13 @@ const numberedLists = document.querySelectorAll('.ui.numbered-list')
 
 for (let i=0; (numberedLists !== null) && (i < numberedLists.length); i++){
     let currNo = 1;
-    let lists = numberedLists[i].getElementsByClassName('list');
+    let lists = numberedLists[i].getElementsByClassName('listItem');
     for (let j=0; (lists !== null) && (j < lists.length); j++) {
-        let title = lists[j].getElementsByClassName('sub-heading');
-        let desc = lists[j].getElementsByClassName('description');
+        let title = lists[j].getElementsByClassName('ui sub-heading');
+        let desc = lists[j].getElementsByClassName('ui one-liner-captions');
         if (desc.length === 0){
             title[0].style.marginBottom = 0;
-            let contents = lists[j].getElementsByClassName('content-flex')[0];
+            let contents = lists[j].getElementsByClassName('content')[0];
             contents.style.justifyContent = "center";
         }
         const itemNo = document.createElement("div");
