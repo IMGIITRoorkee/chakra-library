0dca664952b9c4673dead70e086f78002e82bd49
Radhika <radhika@cs.iitr.ac.in>
2022-01-01 13:28:03 +0530

    Fix textCard to reuse components

--- library/javascript/components/link.js
+++ library/javascript/components/link.js
@@ -1,17 +1,17 @@
-let arrowLinkTags = document.querySelectorAll(".ui.link.arrow-link")
-let imagePath = "../../library/assets/icons/right-arrow.svg";
-let imagePathInverse =
-  "../../library/assets/icons/right-arrow-inverted.svg";
+let arrowLinkTags = document.querySelectorAll('.ui.link.arrow-link')
+let imagePath = '../../library/assets/icons/right-arrow.svg'
+let imagePathInverse = '../../library/assets/icons/right-arrow-inverted.svg'
+let imagePathBlue = '../../library/assets/icons/arrow-icon.svg'
+for (arrowLinkTag of arrowLinkTags) {
+  let image = document.createElement('img')
 
-for(arrowLinkTag of arrowLinkTags){
-    let image = document.createElement("img");
+  if (arrowLinkTag.className === 'ui link arrow-link') {
+    image.setAttribute('src', imagePath)
+  } else if (arrowLinkTag.className === 'ui link arrow-link inverted') {
+    image.setAttribute('src', imagePathInverse)
+  } else if (arrowLinkTag.className === 'ui link arrow-link blue') {
+    image.setAttribute('src', imagePathBlue)
+  }
 
-if (arrowLinkTag.className === "ui link arrow-link" ){
-
-    image.setAttribute("src", imagePath);
-}else if(arrowLinkTag.className === "ui link arrow-link inverted"){
-    image.setAttribute("src", imagePathInverse)
-}
-
-arrowLinkTag.append(image)
+  arrowLinkTag.append(image)
 }
