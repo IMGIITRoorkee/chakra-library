c08e1f8563a66e3e5ba43dfc162874d26d3e7867
mrstark14 <sanjeet_m@ch.iitr.ac.in>
2022-01-03 17:59:55 +0530

    Add students-placeholder

---
+++ library/javascript/components/students-placeholder.js
@@ -0,0 +1,9 @@
+function setNewImage() {
+  document.getElementById("img1").src="../../library/assets/icons/for_students_hover-icon.svg";
+  document.getElementById("headLink").style.textDecoration = "underline";
+}
+
+function setOldImage() {
+  document.getElementById("img1").src="../../library/assets/icons/for_students-icon.svg";
+  document.getElementById("headLink").style.textDecoration = "none";
+}
