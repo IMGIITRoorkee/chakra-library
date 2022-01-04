d95ded13ca52db0dc06d6b8e7b78f97f2d5a0cf5
Radhika <radhika@cs.iitr.ac.in>
2022-01-04 18:31:39 +0530

    Fix profile-placeholder

--- library/javascript/components/students-placeholder.js
+++
@@ -1,9 +0,0 @@
-function setNewImage() {
-  document.getElementById("img1").src="../../library/assets/icons/for_students_hover-icon.svg";
-  document.getElementById("headLink").style.textDecoration = "underline";
-}
-
-function setOldImage() {
-  document.getElementById("img1").src="../../library/assets/icons/for_students-icon.svg";
-  document.getElementById("headLink").style.textDecoration = "none";
-}
