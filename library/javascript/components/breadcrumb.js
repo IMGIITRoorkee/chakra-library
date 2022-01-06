c5d39f08169b66b07082359db4b84490e9152460
Saurabh <saurabhguptajpr@yahoo.in>
2022-01-06 21:22:59 +0530

    :hammer: [title-box] Make page title box responsive

--- library/javascript/components/breadcrumb.js
+++ library/javascript/components/breadcrumb.js
@@ -1,42 +1,42 @@
-var src = "https://cmsredesign.channeli.in/"
+var src = '../../'
 
-const imagePath = src + "library/assets/images/breadcrumbs/right-arrow.svg";
+const imagePath = src + 'library/assets/images/breadcrumbs/right-arrow.svg'
 
-const breadcrumbs = document.querySelectorAll(".ui.breadcrumb");
+const breadcrumbs = document.querySelectorAll('.ui.breadcrumb')
 for (let i = 0; breadcrumbs !== null && i < breadcrumbs.length; i++) {
-  const breadSize = breadcrumbs[i].getBoundingClientRect();
-  const breadWidth = breadSize.width;
-  const parent = breadcrumbs[i].parentElement;
-  const parentWidth = parent.getBoundingClientRect().width;
+  const breadSize = breadcrumbs[i].getBoundingClientRect()
+  const breadWidth = breadSize.width
+  const parent = breadcrumbs[i].parentElement
+  const parentWidth = parent.getBoundingClientRect().width
 
   if (breadWidth >= parentWidth) {
-    const innerChildren = breadcrumbs[i].children;
+    const innerChildren = breadcrumbs[i].children
     if (innerChildren.length > 3) {
-      const newInnerChildren = [];
-      newInnerChildren.push(innerChildren[0], innerChildren[1]);
-      const newDummyChild = document.createElement("a");
-      newDummyChild.className = "section";
-      newDummyChild.innerHTML = "...";
-      newInnerChildren.push(newDummyChild);
+      const newInnerChildren = []
+      newInnerChildren.push(innerChildren[0], innerChildren[1])
+      const newDummyChild = document.createElement('a')
+      newDummyChild.className = 'section'
+      newDummyChild.innerHTML = '...'
+      newInnerChildren.push(newDummyChild)
       newInnerChildren.push(
         innerChildren[innerChildren.length - 2],
         innerChildren[innerChildren.length - 1]
-      );
-      breadcrumbs[i].innerHTML = "";
-      breadcrumbs[i].replaceChildren(...newInnerChildren);
+      )
+      breadcrumbs[i].innerHTML = ''
+      breadcrumbs[i].replaceChildren(...newInnerChildren)
     }
   }
 
-  const arrowDividers = breadcrumbs[i].getElementsByClassName("divider arrow");
+  const arrowDividers = breadcrumbs[i].getElementsByClassName('divider arrow')
   for (let j = 0; arrowDividers !== null && j < arrowDividers.length; j++) {
-    arrowDividers[j].innerHTML = "";
-    const arrow = document.createElement("img");
-    arrow.setAttribute("src", imagePath);
-    arrowDividers[j].appendChild(arrow);
+    arrowDividers[j].innerHTML = ''
+    const arrow = document.createElement('img')
+    arrow.setAttribute('src', imagePath)
+    arrowDividers[j].appendChild(arrow)
   }
 
-  const slashDividers = breadcrumbs[i].getElementsByClassName("divider slash");
+  const slashDividers = breadcrumbs[i].getElementsByClassName('divider slash')
   for (let j = 0; slashDividers !== null && j < slashDividers.length; j++) {
-    slashDividers[j].innerHTML = "/";
+    slashDividers[j].innerHTML = '/'
   }
 }
--- library/javascript/components/link.js
+++ library/javascript/components/link.js
@@ -1,9 +1,9 @@
-var src = "https://cmsredesign.channeli.in/";
+var src = '../../'
 
 let arrowLinkTags = document.querySelectorAll('.ui.link.arrow-link')
-let imagePath = src+"library/assets/icons/right-arrow.svg";
-let imagePathInverse = src+"library/assets/icons/right-arrow-inverted.svg";
-let imagePathBlue = src+"library/assets/icons/arrow-icon.svg";
+let imagePath = src + 'library/assets/icons/right-arrow.svg'
+let imagePathInverse = src + 'library/assets/icons/right-arrow-inverted.svg'
+let imagePathBlue = src + 'library/assets/icons/arrow-icon.svg'
 for (arrowLinkTag of arrowLinkTags) {
   let image = document.createElement('img')
 
