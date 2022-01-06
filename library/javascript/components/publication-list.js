d45652580e79f983094a774f1719a4f23eef7621
rish9101 <rranjan@cs.iitr.ac.in>
2022-01-06 08:29:53 -0500

    Add new class to publication list Item

--- library/javascript/components/publication-list.js
+++ library/javascript/components/publication-list.js
@@ -1,7 +1,7 @@
 const lists = document.querySelectorAll('.ui.publication-list')
 
 for (let i=0; (lists !== null) && (i < lists.length); i++){
-    let listItems = lists[i].getElementsByClassName('listItem');
+    let listItems = lists[i].getElementsByClassName('publicationListItem');
     for (let j=0; (listItems !== null) && (j < listItems.length); j++){
         const divider = document.createElement('hr')
         listItems[j].appendChild(divider);
