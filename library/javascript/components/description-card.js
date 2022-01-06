e93112395170fbccb5f269a30c83e7798d0df738
rish9101 <rranjan@cs.iitr.ac.in>
2022-01-06 13:58:52 -0500

    Fix Description Card JS

--- library/javascript/components/description-card.js
+++ library/javascript/components/description-card.js
@@ -1,8 +1,23 @@
-let descriptionCardInverted = document.querySelectorAll(
-  ".ui.description-card.inverted"
-);
+let decCardContainers = document.querySelectorAll(
+  '.ui.description-card-container'
+)
 
-for (let card of descriptionCardInverted) {
-  let arrows = card.getElementsByClassName("link");
-  arrows[0].className += " inverted";
+function invertedDescCards (descCardContainer) {
+  let cards = descCardContainer.getElementsByClassName('description-card')
+
+  for (let i = 0; i < cards.length; ++i) {
+    if (i % 2 === 0) {
+      cards[i].className += ' inverted'
+      let arrows = cards[i].getElementsByClassName('link')
+      arrows[0].className += ' inverted'
+      let heading = cards[i].getElementsByClassName('section-heading')
+      heading[0].className += ' inverted'
+      let description = cards[i].getElementsByClassName('description')
+      description[0].className += ' inverted'
+    }
+  }
+}
+
+for (let cardContainer of decCardContainers) {
+  invertedDescCards(cardContainer)
 }
