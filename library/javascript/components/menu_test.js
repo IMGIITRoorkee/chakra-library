e8fe99b311eaa63dbf3dbdb4c1620f745e197fe2
Paritosh_Kabra <paritosh_k@cs.iitr.ac.in>
2022-01-07 22:25:00 +0530

    Fix Event Handling in Sub-Menus

--- library/javascript/components/menu_test.js
+++ library/javascript/components/menu_test.js
@@ -3,31 +3,46 @@ let menu_container_list = document.querySelectorAll('.menuInnerContainer');
 let node_sp_parent = document.querySelectorAll('nav.blue > ul');
 let menu_parent_nodes = document.querySelectorAll('.menuParentNode > a');
 let menu_parent_nodes_ul = document.querySelectorAll('.menuParentNode > .menuParentNodeUL');
-
-console.log(special_node_list);
+let nav = document.querySelector('nav');
 
 for (let i = 0; i < special_node_list.length; i++) {
     special_node_list[i].addEventListener('mouseover', () => {
+        if (active_sp_index !== null) {
+            menu_container_list[active_sp_index].classList.remove('visible');
+        }
+        active_sp_index = i;
         menu_container_list[i].classList.add('visible');
-    })
-    node_sp_parent[i].addEventListener('mouseleave', () => {
-        menu_container_list[i].classList.remove('visible');
         menu_parent_nodes_ul.forEach(ele => {
             ele.classList.remove(('parentVisible'));
         })
     })
 }
+let active_parent_node = null;
+let active_parent_index = null;
+let active_sp_index = null;
+
 menu_parent_nodes.forEach((element, index) => {
     element.addEventListener('mouseover', () => {
+        if (active_parent_node !== null) {
+            menu_parent_nodes_ul[active_parent_index].classList.remove('parentVisible');
+            active_parent_node.classList.remove('boldParentNode');
+        }
+        active_parent_node = element;
+        active_parent_index = index;
         menu_parent_nodes_ul[index].classList.add('parentVisible');
+        active_parent_node.classList.add('boldParentNode');
     })
-    element.addEventListener('mouseleave', () => {
-        menu_parent_nodes_ul[index].classList.remove('parentVisible');
-    })
-    menu_parent_nodes_ul[index].addEventListener('mouseover', () => {
-        menu_parent_nodes_ul[index].classList.add('parentVisible');
-    });
-    menu_parent_nodes_ul[index].addEventListener('mouseleave', () => {
-        menu_parent_nodes_ul[index].classList.remove('parentVisible');
-    });
 });
+
+
+nav.addEventListener('mouseleave', () => {
+    menu_container_list.forEach(ele => {
+        ele.classList.remove('visible');
+    })
+    menu_parent_nodes_ul.forEach(ele => {
+        ele.classList.remove('parentVisible');
+    })
+    menu_parent_nodes.forEach(ele => {
+        ele.classList.remove('boldParentNode');
+    })
+})
