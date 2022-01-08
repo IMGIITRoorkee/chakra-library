81c2642382165129d5946bb360181781540acaba
Paritosh_Kabra <paritosh_k@cs.iitr.ac.in>
2022-01-08 18:27:59 +0530

    Fix PC View of Navbar

--- library/javascript/components/menu_test.js
+++ library/javascript/components/menu_test.js
@@ -1,5 +1,6 @@
 let special_node_list = document.querySelectorAll('div.menuSpecialNode');
-let menu_container_list = document.querySelectorAll('.menuInnerContainer');
+let menu_containers_list = document.querySelectorAll('.menuInnerContainer');
+let menu_container_items = document.querySelectorAll('.menuInnerContainer li');
 let node_sp_parent = document.querySelectorAll('nav.blue > ul');
 let menu_parent_nodes = document.querySelectorAll('.menuParentNode > a');
 let menu_parent_nodes_ul = document.querySelectorAll('.menuParentNode > .menuParentNodeUL');
@@ -8,41 +9,46 @@ let nav = document.querySelector('nav');
 for (let i = 0; i < special_node_list.length; i++) {
     special_node_list[i].addEventListener('mouseover', () => {
         if (active_sp_index !== null) {
-            menu_container_list[active_sp_index].classList.remove('visible');
+            menu_containers_list[active_sp_index].classList.remove('visible');
         }
         active_sp_index = i;
-        menu_container_list[i].classList.add('visible');
+        menu_containers_list[i].classList.add('visible');
         menu_parent_nodes_ul.forEach(ele => {
             ele.classList.remove(('parentVisible'));
         })
     })
 }
 let active_parent_node = null;
-let active_parent_index = null;
 let active_sp_index = null;
 
-menu_parent_nodes.forEach((element, index) => {
+menu_container_items.forEach((element, index) => {
     element.addEventListener('mouseover', () => {
+        console.log(element);
         if (active_parent_node !== null) {
-            menu_parent_nodes_ul[active_parent_index].classList.remove('parentVisible');
-            active_parent_node.classList.remove('boldParentNode');
-        }
-        active_parent_node = element;
-        active_parent_index = index;
-        menu_parent_nodes_ul[index].classList.add('parentVisible');
-        active_parent_node.classList.add('boldParentNode');
-    })
-});
+            active_parent_node.childNodes[3].classList.remove('parentVisible');
+            active_parent_node.classList.remove('activeParentNode');
+            active_parent_node.childNodes[1].childNodes[3].classList.remove('activeParentNode');
 
+        }
+        if (element.classList[0] === 'menuParentNode') {
+            element.childNodes[1].childNodes[3].classList.add('activeParentNode');
+            active_parent_node = element;
+            element.childNodes[3].classList.add('parentVisible');
+            element.classList.add('activeParentNode');
+        } else {
+            active_parent_node = null;
+        }
+    })
+})
 
 nav.addEventListener('mouseleave', () => {
-    menu_container_list.forEach(ele => {
+    menu_containers_list.forEach(ele => {
         ele.classList.remove('visible');
     })
     menu_parent_nodes_ul.forEach(ele => {
         ele.classList.remove('parentVisible');
     })
-    menu_parent_nodes.forEach(ele => {
-        ele.classList.remove('boldParentNode');
+    menu_container_items.forEach(ele => {
+        ele.classList.remove('activeParentNode');
     })
 })
