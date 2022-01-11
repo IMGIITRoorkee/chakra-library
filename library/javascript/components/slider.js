e9043a616a76b425f3123ad7bc140a5e5776ff48
Radhika <radhika@cs.iitr.ac.in>
2022-01-11 21:36:44 +0530

    Fix slider JS
    Co-authored-by: Rishi Ranjan <rish9101@users.noreply.github.com>

--- library/javascript/components/slider.js
+++ library/javascript/components/slider.js
@@ -1,164 +1,175 @@
-var src = 'https://cmsredesign.channeli.in/'
-var rightArrow = src + 'library/assets/icons/slider-right-arrow.svg'
-var leftArrow = src + 'library/assets/icons/slider-left-arrow.svg'
+const src = 'https://cmsredesign.channeli.in/'
+const rightArrow = src + 'library/assets/icons/slider-right-arrow.svg'
+const leftArrow = src + 'library/assets/icons/slider-left-arrow.svg'
 
 // Hash function
+// Returns a hash of argument length
 function makeHash (length) {
-  var result = ''
-  var characters =
+  let result = ''
+  const characters =
     'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-  var charactersLength = characters.length
-  for (var i = 0; i < length; i++) {
+  const charactersLength = characters.length
+  for (let i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength))
   }
   return result
 }
 
-const flushSlides = sliderHash => {
-  const slidesElement = document.getElementById(sliderHash)
-  const container = slidesElement.getElementsByClassName('container')[0]
+// THIS FUNCTION REORDERS SLIDES by either inserting the last slide to first or
+// first slide to last
+const reorderSlides = (sliderHash, numCards, isLeft) => {
+  const sliderElement = document.getElementById(sliderHash)
+  const container = sliderElement.getElementsByClassName('container')[0]
   const cards = container.children
 
-  for (var i = 0; cards !== null && i < cards.length; i++)
-    [(cards[i].style.display = 'none')]
-}
+  const totalNumberOfCards = cards.length
 
-const reorderSlides = (sliderHash, numberOfCardsRendered, isLeft) => {
-  const slidesElement = document.getElementById(sliderHash)
-  const container = slidesElement.getElementsByClassName('container')[0]
-  const cards = container.children
-
-  const numberOfCards = cards.length
-
-  if (numberOfCardsRendered < 1) {
+  if (numCards < 1) {
     return
   }
 
   // Reorder
   if (isLeft) {
     // Put last card to the top
-    const lastCard = cards[numberOfCards - 1]
+    if (totalNumberOfCards > numCards) {
+      cards[numCards - 1].style.display = 'none'
+    }
+    const lastCard = cards[totalNumberOfCards - 1]
+    if (lastCard.classList.contains('image-description-card')) {
+      lastCard.style.display = 'flex'
+    } else {
+      lastCard.style.display = 'block'
+    }
     container.insertBefore(lastCard, container.firstChild)
   } else {
     // Put first card to the bottom
     const firstChild = cards[0]
+    if (totalNumberOfCards > numCards) {
+      firstChild.style.display = 'none'
+    }
+    cards[numCards].style.display = 'block'
+    if (cards[numCards].classList.contains('image-description-card')) {
+      cards[numCards].style.display = 'flex'
+    } else {
+      cards[numCards].style.display = 'block'
+    }
     container.appendChild(firstChild)
   }
 }
 
-const renderSlides = (sliderHash, numberOfCardsRendered) => {
-  const slidesElement = document.getElementById(sliderHash)
-  const container = slidesElement.getElementsByClassName('container')[0]
+const renderSlides = (hash, numberOfCards) => {
+  const sliderElement = document.getElementById(hash)
+  const container = sliderElement.getElementsByClassName('container')[0]
   const cards = container.children
-
-  const numberOfCards = cards.length
-
-  for (var i = 0; i < numberOfCards && i < numberOfCardsRendered; i++) {
-    if (cards[i].classList.contains('image-description-card')) {
-      cards[i].style.display = 'flex'
-    } else {
-      cards[i].style.display = 'block'
-    }
+  for (let i = 0; i < cards.length; ++i) {
+    if (i < numberOfCards) {
+      if (cards[i].classList.contains('image-description-card')) {
+        cards[i].style.display = 'flex'
+      } else {
+        cards[i].style.display = 'block'
+      }
+    } else cards[i].style.display = 'none'
   }
 }
 
-const numberOfCardsDisplayed = sliderHash => {
-  const slidesElement = document.getElementById(sliderHash)
+// FUNCTION TO CALCULATE THE NUMBER OF CARDS
 
-  const container = slidesElement.getElementsByClassName('container')[0]
+const numberOfCardsToDisplay = sliderHash => {
+  const sliderElement = document.getElementById(sliderHash)
+
+  const vw = Math.max(
+    document.documentElement.clientWidth || 0,
+    window.innerWidth || 0
+  )
+  const px = Math.ceil((vw * 10) / 100)
+
+  const container = sliderElement.getElementsByClassName('container')[0]
   const card = container.firstElementChild
-
-  var numberOfCards = Math.floor(slidesElement.clientWidth / card.clientWidth)
-  return numberOfCards > 1 ? numberOfCards - 1 : 1
+  console.log(
+    'Slides Element Width',
+    sliderElement.clientWidth - px,
+    card.offsetWidth
+  )
+  const numberOfCards = Math.floor(
+    (sliderElement.clientWidth - px) / card.offsetWidth
+  )
+  const ret = numberOfCards > 1 ? numberOfCards : 1
+  console.log('RET CARDS', ret)
+  return ret
 }
+
+const sliderMap = {}
+
+document.addEventListener('DOMContentLoaded', function (e) {
+  const sliders = document.querySelectorAll('.ui.slider')
+  for (let i = 0; i < sliders.length; ++i) {
+    const hash = makeHash(10)
+    sliders[i].id = hash
+    sliderMap[hash] = 1
+
+    const container = document.createElement('div')
+    container.className = 'container'
+
+    let card = sliders[i].firstChild
+
+    while (card) {
+      container.appendChild(card)
+      card = sliders[i].firstChild
+    }
+
+    const leftArrowContainer = document.createElement('div')
+    leftArrowContainer.className = 'left-arrow-container'
+
+    const leftArrowElement = document.createElement('img')
+    leftArrowElement.className = 'arrow'
+    leftArrowElement.setAttribute('src', leftArrow)
+    leftArrowContainer.appendChild(leftArrowElement)
+
+    const rightArrowContainer = document.createElement('div')
+    rightArrowContainer.className = 'right-arrow-container'
+
+    const rightArrowElement = document.createElement('img')
+    rightArrowElement.className = 'arrow'
+    rightArrowElement.setAttribute('src', rightArrow)
+    rightArrowContainer.appendChild(rightArrowElement)
+
+    sliders[i].appendChild(leftArrowContainer)
+    sliders[i].appendChild(container)
+    sliders[i].appendChild(rightArrowContainer)
+
+    leftArrowElement.addEventListener('click', function (e) {
+      e.stopPropagation()
+      e.preventDefault()
+
+      if (e.target) {
+        reorderSlides(hash, sliderMap[hash], true)
+      }
+    })
+
+    rightArrowElement.addEventListener('click', function (e) {
+      e.stopPropagation()
+      e.preventDefault()
+
+      if (e.target) {
+        reorderSlides(hash, sliderMap[hash], false)
+      }
+    })
+
+    renderSlides(hash, sliderMap[hash])
+    sliderMap[hash] = numberOfCardsToDisplay(hash)
+    renderSlides(hash, sliderMap[hash])
+  }
+})
 
 window.onresize = () => {
   const sliders = document.querySelectorAll('.ui.slider')
-
-  for (var i = 0; sliders !== null && i < sliders.length; i++) {
+  if (sliders === null) {
+    return
+  }
+  for (let i = 0; i < sliders.length; i++) {
     const hash = sliders[i].id
-    numberOfCardsRendered = numberOfCardsDisplayed(hash)
-    flushSlides(hash)
-    renderSlides(hash, numberOfCardsRendered)
+    sliderMap[hash] = numberOfCardsToDisplay(hash)
+
+    renderSlides(hash, sliderMap[hash])
   }
-}
-
-// Assign a unique hash to the sliders
-var sliders = document.querySelectorAll('.ui.slider')
-for (var i = 0; sliders !== null && i < sliders.length; i++) {
-  var numberOfCardsRendered = 1 // Default
-
-  const hash = makeHash(20)
-  sliders[i].id = hash
-
-  const container = document.createElement('div')
-  container.className = 'container'
-
-  var card = sliders[i].firstChild
-
-  while (card) {
-    container.appendChild(card)
-    card = sliders[i].firstChild
-  }
-
-  const leftArrowContainer = document.createElement('div')
-  leftArrowContainer.className = 'left-arrow-container'
-
-  const leftArrowElement = document.createElement('img')
-  leftArrowElement.className = 'arrow'
-  leftArrowElement.setAttribute('src', leftArrow)
-  leftArrowContainer.appendChild(leftArrowElement)
-
-  const rightArrowContainer = document.createElement('div')
-  rightArrowContainer.className = 'right-arrow-container'
-
-  const rightArrowElement = document.createElement('img')
-  rightArrowElement.className = 'arrow'
-  rightArrowElement.setAttribute('src', rightArrow)
-  rightArrowContainer.appendChild(rightArrowElement)
-
-  sliders[i].appendChild(leftArrowContainer)
-  sliders[i].appendChild(container)
-  sliders[i].appendChild(rightArrowContainer)
-
-  numberOfCardsRendered = numberOfCardsDisplayed(hash)
-  flushSlides(hash)
-  renderSlides(hash, numberOfCardsRendered)
-
-  leftArrowElement.addEventListener('click', function (e) {
-    e.stopPropagation()
-    e.preventDefault()
-
-    if (e.target) {
-      // Flush Slides
-      // Reorder slides
-      // Render the top `m` slides
-
-      flushSlides(hash)
-      reorderSlides(hash, numberOfCardsRendered, true)
-      renderSlides(hash, numberOfCardsRendered)
-    }
-  })
-
-  rightArrowElement.addEventListener('click', function (e) {
-    e.stopPropagation()
-    e.preventDefault()
-
-    if (e.target) {
-      // Flush Slides
-      // Reorder slides
-      // Render the top `m` slides
-
-      flushSlides(hash)
-      reorderSlides(hash, numberOfCardsRendered, false)
-      renderSlides(hash, numberOfCardsRendered)
-    }
-  })
-}
-
-for (var i = 0; sliders !== null && i < sliders.length; i++) {
-  const hash = sliders[i].id
-  numberOfCardsRendered = numberOfCardsDisplayed(hash)
-  flushSlides(hash)
-  renderSlides(hash, numberOfCardsRendered)
 }
