91a1a481dd55b547a1a099e19ce13b0f9e45ab1c
Radhika <radhika@cs.iitr.ac.in>
2022-01-13 22:48:34 +0530

    Fix complete info card
    Co-authored-by: Rishi Ranjan <rish9101@users.noreply.github.com>

--- library/javascript/components/link.js
+++ library/javascript/components/link.js
@@ -1,18 +1,13 @@
-var src = 'https://cmsredesign.channeli.in/'
-
 let arrowLinkTags = document.querySelectorAll('.ui.link.arrow-link')
-let imagePath = src + 'library/assets/icons/right-arrow.svg'
-let imagePathInverse = src + 'library/assets/icons/right-arrow-inverted.svg'
-let imagePathBlue = src + 'library/assets/icons/blue-arrow.svg'
 for (arrowLinkTag of arrowLinkTags) {
   let image = document.createElement('img')
-
+  image.className = 'ui icon'
   if (arrowLinkTag.className === 'ui link arrow-link') {
-    image.setAttribute('src', imagePath)
+    image.setAttribute('data-icon', 'right_arrow')
   } else if (arrowLinkTag.className === 'ui link arrow-link inverted') {
-    image.setAttribute('src', imagePathInverse)
+    image.setAttribute('data-icon', 'right_arrow_dark')
   } else if (arrowLinkTag.className === 'ui link arrow-link blue') {
-    image.setAttribute('src', imagePathBlue)
+    image.setAttribute('data-icon', 'blue_arrow')
   }
 
   arrowLinkTag.append(image)
