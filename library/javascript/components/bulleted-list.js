5fd08ab26803b83aed0f678baacf882a2e77255a
Radhika <radhika@cs.iitr.ac.in>
2022-01-14 13:16:12 +0530

    Fix bulleted list js
    Co-authored-by: Rishi Ranjan <rish9101@users.noreply.github.com>

--- library/javascript/components/bulleted-list.js
+++ library/javascript/components/bulleted-list.js
@@ -1,22 +1,19 @@
 const bulletedLists = document.querySelectorAll('.ui.bulleted-list')
 
 for (let i = 0; bulletedLists !== null && i < bulletedLists.length; i++) {
-  //   let Ficon
-  //   if (bulletedLists[i].hasAttribute('data-icon')) {
-  //     icon = bulletedLists[i].getAttribute('data-icon')
-  //   } else {
-  // if (bulletedLists[i].classList.contains('inverted')){
-  //     icon = "arrow_dark";
-  // } else {
-  bulletedLists[i].setAttribute('data-icon', 'blue_arrow')
-  // icon = 'arrow'
-  // }
-  //   }F
-  // let bulletedItems = bulletedLists[i].getElementsByClassName('listItem');
-  // for (let j=0; (bulletedItems !== null) && (j < bulletedItems.length); j++) {
-  //     const image = document.createElement("img");
-  //     image.setAttribute("src", "../../"+paths[icon]);
-  //     image.classList.add("icon_image");
-  //     bulletedItems[j].prepend(image);
-  // }
+  let icon
+  if (bulletedLists[i].hasAttribute('data-icon')) {
+    icon = bulletedLists[i].getAttribute('data-icon')
+  } else {
+    bulletedLists[i].setAttribute('data-icon', 'blue_arrow')
+    icon = 'blue_arrow'
+  }
+  let bulletedItems = bulletedLists[i].getElementsByClassName('listItem')
+  for (let j = 0; bulletedItems !== null && j < bulletedItems.length; j++) {
+    const image = document.createElement('img')
+    image.setAttribute('data-icon', icon)
+    image.className = 'ui icon'
+    image.classList.add('icon_image')
+    bulletedItems[j].prepend(image)
+  }
 }
