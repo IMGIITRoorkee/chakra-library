bd95a2216d4a7062a7bec59cd2962c1bac3c1007
Radhika <radhika@cs.iitr.ac.in>
2022-01-20 13:30:37 +0530

    Use space-between in menu

--- library/javascript/components/header.js
+++ library/javascript/components/header.js
@@ -2,7 +2,6 @@ var html = document.getElementsByTagName('html')[0]
 var small = document.querySelectorAll('.small')
 small.forEach(element => {
   element.addEventListener('click', () => {
-    console.log('Hi')
     html.style.fontSize = '14px'
   })
 })
@@ -10,7 +9,6 @@ small.forEach(element => {
 var medium = document.querySelectorAll('.medium')
 medium.forEach(element => {
   element.addEventListener('click', () => {
-    console.log('Hi')
     html.style.fontSize = '16px'
   })
 })
@@ -18,7 +16,6 @@ medium.forEach(element => {
 var big = document.querySelectorAll('.big')
 big.forEach(element => {
   element.addEventListener('click', () => {
-    console.log('Hi')
     html.style.fontSize = '18px'
   })
 })
