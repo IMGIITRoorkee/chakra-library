2358385b38e21602643d5c829e5032d10c121d39
mrstark14 <sanjeet_m@ch.iitr.ac.in>
2022-05-09 18:30:05 +0530

    Add nested vertical list

---
+++ library/javascript/components/nested-vertical-list.js
@@ -0,0 +1,25 @@
+const url = window.location.href;
+
+function head_click(id) {
+    const element = document.getElementById(id).nextSibling.nextSibling
+    const style = getComputedStyle(element);
+    if (style.display == 'none') {
+        element.style.display = 'block';
+    }
+    else if (style.display == 'block') {
+        element.style.display = 'none';
+    }
+    else {
+        console.log('invalid display style');
+    }
+}
+
+function content_click(id) {
+    const element1 = document.getElementsByClassName('active-list-item')
+    if (element1.length != 0) {
+        element1[0].classList.remove("active-list-item")
+    }
+    const element = document.getElementById(id)
+    element.classList.add("active-list-item")
+    window.location = url + '#' + id + 'd';
+}
