b67520754096122611aea9beb55d45523179bd2f
Sanjeet Manna <sanjeet_m@ch.iitr.ac.in>
2022-05-13 01:06:38 +0530

    Remove horizontal scroll in fac-profile

--- library/javascript/components/nested-vertical-list.js
+++ library/javascript/components/nested-vertical-list.js
@@ -1,40 +1,37 @@
 const url = window.location.href.split('#')[0]
 
-const elements = document.getElementsByClassName('one-liner-captions')
+const vertical_list_nest = document.getElementsByClassName('one-liner-captions')
 
-const elements1 = document.getElementsByClassName('list-sub-heading')
+const list_sub_heading = document.getElementsByClassName('list-sub-heading')
 
-for(let i=0; i<elements.length; i++){
-    var id = elements[i].innerHTML.toLowerCase()
-    elements[i].setAttribute('id', id.replace(/\s/g,'') + 'd')
+for(let i=0; i<vertical_list_nest.length; i++){
+    var id = vertical_list_nest[i].innerHTML.toLowerCase()
+    vertical_list_nest[i].setAttribute('id', id.replace(/\s/g,'') + 'd')
 }
 
-for(let i=0; i<elements1.length; i++){
-    var id = elements1[i].innerHTML.toLowerCase()
-    elements1[i].setAttribute('id', id.replace(/\s/g,''))
+for(let i=0; i<list_sub_heading.length; i++){
+    var id = list_sub_heading[i].innerHTML.toLowerCase()
+    list_sub_heading[i].setAttribute('id', id.replace(/\s/g,''))
 }
 
 function head_click(id) {
-    const element = document.getElementById(id).nextSibling.nextSibling
-    const style = getComputedStyle(element);
+    const sub_heading = document.getElementById(id).nextSibling.nextSibling
+    const style = getComputedStyle(sub_heading);
     if (style.display == 'none') {
-        element.style.display = 'block';
-    }
-    else if (style.display == 'block') {
-        element.style.display = 'none';
+        sub_heading.style.display = 'block';
     }
     else {
-        console.log('invalid display style');
+        sub_heading.style.display = 'none';
     }
 }
 
 function content_click(id) {
-    const element1 = document.getElementsByClassName('active-list-item')
-    if (element1.length != 0) {
-        element1[0].classList.remove("active-list-item")
+    const active_item = document.getElementsByClassName('active-list-item')
+    if (active_item.length !== 0) {
+        active_item[0].classList.remove("active-list-item")
     }
-    const element = document.getElementById(id)
-    element.classList.add("active-list-item")
+    const new_active_item = document.getElementById(id)
+    new_active_item.classList.add("active-list-item")
     var link = url + '#' + id;
     window.location = link.slice(0, -1)
 }
