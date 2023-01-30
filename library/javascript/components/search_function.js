81bad414d35cfb46324d21261534d9868aef8d75
Paritosh Kabra <paritosh_k@cs.iitr.ac.in>
2023-01-30 22:13:52 +0530

    Fix Pagination Bug in Search Function

--- library/javascript/components/search_function.js
+++ library/javascript/components/search_function.js
@@ -1,77 +1,85 @@
-var search_input = document.getElementById('search-input')
-if (search_input != null) {
-  search_input.addEventListener('keydown', e => {
-    if (e.key === 'Enter') {
-      search(e.target.value, 1)
-    }
-  })
+var search_input = document.getElementById('search-input');
 
-  function search(words, currentPageID) {
-    if (document.getElementById('search-modal') != null)
-      document.getElementById('search-modal').remove()
-    if (document.getElementById('pages-list') != null)
-      document.getElementById('pages-list').remove()
-    const URL = `https://elastic.channeli.in/xmlpages-000001/_search`
-    const paginationLimit = 10
-    let data = {
-      "sort": [
-        {
-          "_score": {
-            "order": "desc"
-          }
-        }
-      ],
-      "size": paginationLimit,
-      "from": (currentPageID - 1) * paginationLimit,
-      "query": {
-        "query_string": {
-          "default_field": "search_keywords",
-          "query": words
-        }
-      }
+var startPageNumber = 1;
+var currPageListId;
+var previousPageListId;
+var searchResult;
+
+function uuidv4() {
+  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  );
+}
+
+if (search_input != null) {
+  search_input.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+      search(e.target.value, 1);
     }
-    let headers = new Headers()
-    headers.append('Content-Type', 'application/json')
-    headers.append('Accept', 'application/json')
-    headers.append('Origin', 'elastic.channeli.in')
+  });
+  function search(words, currentPageID) {
+    const PAGINATION_LIMIT = 10;
+    if (startPageNumber === currentPageID && currentPageID != 1) {
+      startPageNumber -= 1;
+    }
+    const URL = `https://elastic.channeli.in/xmlpages-000001/_search`;
+    let data = {
+      sort: [
+        {
+          _score: {
+            order: 'desc',
+          },
+        },
+      ],
+      size: PAGINATION_LIMIT,
+      from: (currentPageID - 1) * PAGINATION_LIMIT,
+      query: {
+        query_string: {
+          default_field: 'search_keywords',
+          query: words,
+        },
+      },
+    };
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    headers.append('Accept', 'application/json');
+    headers.append('Origin', 'elastic.channeli.in');
     fetch(URL, {
       mode: 'cors',
       credentials: 'include',
       method: 'POST',
       headers: headers,
-      body: JSON.stringify(data)
+      body: JSON.stringify(data),
     })
-      .then(data => {
-        return data.json()
+      .then((data) => {
+        return data.json();
       })
-      .then(res => {
-        var max_pages = res.hits.total.value
-        var searchResult = document.createElement('div')
-        searchResult.setAttribute('id', 'pages-list')
-
+      .then((res) => {
+        var max_pages = res.hits.total.value;
+        if (searchResult === undefined) searchResult = document.createElement('div');
+        searchResult.setAttribute('id', 'results-list');
+        searchResult.style.zIndex = '10000';
+        searchResult.style.width = '100vw';
+        searchResult.style.height = '100vh';
+        searchResult.style.paddingTop = '5vh';
+        searchResult.style.position = 'absolute';
+        searchResult.style.background = 'white';
+        searchResult.style.top =
+          document
+            .getElementsByClassName('main-head')[0]
+            .getBoundingClientRect().bottom + 'px';
         if (max_pages === 0) {
-          searchResult.innerHTML += `<div class="description-container">
+          searchResult.innerHTML = `<div class="description-container">
           <div class="ui description center">
-            No search results 
+          No search results 
           </div>
-        </div>`
-          searchResult.style.zIndex = '999'
-          searchResult.style.width = '100vw'
-          searchResult.style.height = '100vh'
-          searchResult.style.paddingTop = '10vh'
-          searchResult.style.position = 'absolute'
-          searchResult.style.background = 'white'
-          searchResult.style.top =
-            document
-              .getElementsByClassName('main-head')[0]
-              .getBoundingClientRect().bottom + 'px'
-          document.body.appendChild(searchResult)
+          </div>`;
+          document.body.appendChild(searchResult);
         } else {
-          list_html = ''
+          list_html = '';
           const { hits } = res;
           let { hits: pages } = hits;
-          pages = pages.map(e => e._source)
-          console.log(pages)
+          pages = pages.map((e) => e._source);
           for (let page = 0; page < pages.length; page++) {
             list_html +=
               `<div class="publicationListItem">
@@ -87,10 +95,10 @@ if (search_input != null) {
               pages[page].description +
               `</div>
                     <hr>
-                    </div>`
+                    </div>`;
           }
 
-          searchResult.innerHTML +=
+          searchResult.innerHTML =
             `<div class="ui full-width-container" style="max-height: 75vh; overflow-y: auto;">
                 <div class="ui section-heading">
                   Showing search results for <span style="color:rgba(26, 118, 187, 0.87)">${words}</span>
@@ -103,9 +111,9 @@ if (search_input != null) {
                     <button class="pagination-button" id="prev-button" aria-label="Previous page" title="Previous page">
                     &lt;
                     </button>
-
+                    
                     <div id="pagination-numbers">
-
+                    
                     </div>
 
                     <button class="pagination-button" id="next-button" aria-label="Next page" title="Next page">
@@ -114,108 +122,111 @@ if (search_input != null) {
                 </nav>
                 </div>
                 </div>
-                <script  src="https://cmsredesign.channeli.in/library/javascript/components/publication-list.js"></script>`
+                <script  src="https://cmsredesign.channeli.in/library/javascript/components/publication-list.js"></script>`;
 
-          searchResult.style.zIndex = '999'
-          searchResult.style.width = '100vw'
-          searchResult.style.height = '100vh'
-          searchResult.style.paddingTop = '5vh'
-          searchResult.style.position = 'absolute'
-          searchResult.style.background = 'white'
-          searchResult.style.top =
-            document
-              .getElementsByClassName('main-head')[0]
-              .getBoundingClientRect().bottom + 'px'
-          document.body.appendChild(searchResult)
+          document.body.appendChild(searchResult);
           const paginationNumbers =
-            document.getElementById('pagination-numbers')
-          var paginatedList = document.getElementById('paginated-list')
-          var listItems = document.querySelectorAll('.publicationListItem')
-          const nextButton = document.getElementById('next-button')
-          const prevButton = document.getElementById('prev-button')
-          const pageCount = Math.ceil(max_pages / paginationLimit)
+            document.getElementById('pagination-numbers');
+          var paginatedList = document.getElementById('paginated-list');
+          var listItems = document.querySelectorAll('.publicationListItem');
+          const nextButton = document.getElementById('next-button');
+          const prevButton = document.getElementById('prev-button');
+          let maxPageCount = Math.ceil(max_pages / PAGINATION_LIMIT);
 
-          let currentPage = currentPageID
-          const disableButton = button => {
-            button.classList.add('paginated-disabled')
-            button.setAttribute('disabled', true)
+          if (currentPageID === startPageNumber + PAGINATION_LIMIT - 1) {
+            startPageNumber += 1;
           }
-          const enableButton = button => {
-            button.classList.remove('paginated-disabled')
-            button.removeAttribute('disabled')
-          }
+          let currentPage = currentPageID;
+          const disableButton = (button) => {
+            button.classList.add('paginated-disabled');
+            button.setAttribute('disabled', true);
+          };
+          const enableButton = (button) => {
+            button.classList.remove('paginated-disabled');
+            button.removeAttribute('disabled');
+          };
           const handlePageButtonsStatus = () => {
             if (currentPage === 1) {
-              disableButton(prevButton)
+              disableButton(prevButton);
             } else {
-              enableButton(prevButton)
+              enableButton(prevButton);
             }
 
-            if (pageCount === currentPage) {
-              disableButton(nextButton)
+            if (maxPageCount === currentPage) {
+              disableButton(nextButton);
             } else {
-              enableButton(nextButton)
+              enableButton(nextButton);
             }
-          }
+          };
 
           const handleActivePageNumber = () => {
-            document.querySelectorAll('.pagination-number').forEach(button => {
-              button.classList.remove('paginated-active')
-              const pageIndex = Number(button.getAttribute('page-index'))
-              if (pageIndex == currentPage) {
-                button.classList.add('paginated-active')
-              }
-            })
-          }
+            document
+              .querySelectorAll('.pagination-number')
+              .forEach((button) => {
+                button.classList.remove('paginated-active');
+                const pageIndex = Number(button.getAttribute('page-index'));
+                if (pageIndex == currentPage) {
+                  button.classList.add('paginated-active');
+                }
+              });
+          };
 
-          const appendPageNumber = index => {
-            const pageNumber = document.createElement('button')
-            pageNumber.className = 'pagination-number'
-            pageNumber.innerHTML = index
-            pageNumber.setAttribute('page-index', index)
-            pageNumber.setAttribute('aria-label', 'Page ' + index)
+          const appendPageNumber = (index) => {
+            const pageNumber = document.createElement('button');
+            pageNumber.className = 'pagination-number';
+            pageNumber.innerHTML = index;
+            pageNumber.setAttribute('page-index', index);
+            pageNumber.setAttribute('aria-label', 'Page ' + index);
 
-            paginationNumbers.appendChild(pageNumber)
-          }
+            paginationNumbers.appendChild(pageNumber);
+          };
 
           const getPaginationNumbers = () => {
-            for (let i = 1; i <= pageCount; i++) {
-              appendPageNumber(i)
+            for (
+              let i = startPageNumber;
+              i <
+              Math.min(startPageNumber + PAGINATION_LIMIT, 1 + maxPageCount);
+              i++
+            ) {
+              appendPageNumber(i);
             }
-          }
+          };
 
-          const setCurrentPage = pageNum => {
-            currentPage = pageNum
+          const setCurrentPage = (pageNum) => {
+            currentPage = pageNum;
 
-            handleActivePageNumber()
-            handlePageButtonsStatus()
-          }
+            handleActivePageNumber();
+            handlePageButtonsStatus();
+          };
 
-          pager = () => {
-            getPaginationNumbers()
-            setCurrentPage(currentPage)
+          const pager = () => {
+            getPaginationNumbers();
+            setCurrentPage(currentPage);
 
             prevButton.addEventListener('click', () => {
-              search(words, currentPage - 1)
-            })
+              search(words, currentPage - 1);
+            });
 
             nextButton.addEventListener('click', () => {
-              search(words, currentPage + 1)
-            })
+              search(words, currentPage + 1);
+            });
 
-            document.querySelectorAll('.pagination-number').forEach(button => {
-              const pageIndex = Number(button.getAttribute('page-index'))
+            document
+              .querySelectorAll('.pagination-number')
+              .forEach((button) => {
+                const pageIndex = Number(button.getAttribute('page-index'));
 
-              if (pageIndex) {
-                button.addEventListener('click', () => {
-                  search(words, pageIndex)
-                })
-              }
-            })
-          }
+                if (pageIndex) {
+                  button.addEventListener('click', () => {
+                    search(words, pageIndex);
+                  });
+                }
+              });
+          };
 
-          pager()
+          pager();
         }
-      })
+        document.getElementById('search-modal')?.remove();
+      });
   }
 }
